7e5648baa15d545ffe2f066e6ad5f91253ec1c4c
beroncarla <beroncarlaviedma@gmail.com>
2026-01-04 21:33:25 -0300

    creo userRepository

--- src/main/java/com/example/usermanagement/repository/UserRepository.java
+++ src/main/java/com/example/usermanagement/repository/UserRepository.java
@@ -1,4 +1,26 @@
 package com.example.usermanagement.repository;
 
+import com.example.usermanagement.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class UserRepository {
+
+    private List<User> users = new ArrayList<>();
+
+    public void save(User user) {
+        this.users.add(user);
+    }
+
+    public List<User> listAll() {
+        return this.users;
+    }
+
+    public Optional<User> findById(Long id) {
+        return users.stream()
+                .filter(user -> user.id().equals(id))
+                .findFirst();
+    }
 }

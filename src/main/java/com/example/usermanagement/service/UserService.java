8e2d2585cc9801d0bb29b95447b929f3bab65321
beroncarla <beroncarlaviedma@gmail.com>
2026-01-04 21:48:18 -0300

    creo UserService, modifico readme

--- src/main/java/com/example/usermanagement/service/UserService.java
+++ src/main/java/com/example/usermanagement/service/UserService.java
@@ -1,4 +1,25 @@
 package com.example.usermanagement.service;
 
+import com.example.usermanagement.model.User;
+import com.example.usermanagement.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class UserService {
+
+    private UserRepository userRepository;
+
+    public void createUser(Long id, String name, String email) {
+        var user = new User(id, name, email);
+        this.userRepository.save(user);
+    }
+
+    public List<User> getUsers() {
+        return this.userRepository.listAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return this.userRepository.findById(id);
+    }
 }

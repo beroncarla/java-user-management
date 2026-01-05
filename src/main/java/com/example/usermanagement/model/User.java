3a43183846b1543e7bca77febac67d6f3db88771
beroncarla <beroncarlaviedma@gmail.com>
2026-01-04 21:19:35 -0300

    creo clase user

--- src/main/java/com/example/usermanagement/model/User.java
+++ src/main/java/com/example/usermanagement/model/User.java
@@ -1,4 +1,25 @@
 package com.example.usermanagement.model;
 
 public class User {
+    private Long id;
+    private String name;
+    private String email;
+
+    public User(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long id() {
+        return id;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String email() {
+        return email;
+    }
 }
